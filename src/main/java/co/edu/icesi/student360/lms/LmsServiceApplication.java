72a5a3152796da27619bdba88bafae8acdc2c5f4
Kevin Steven Nieto Curaca <kevin.nieto@u.icesi.edu.co>
2026-08-30 05:36:44 -0500

    build: add spring boot service skeleton depending on student360-common
    Same shape as core-service: web, validation, JPA, AOP, actuator with tracing, Flyway confined to the lms schema, springdoc in dev, PORT as the Cloud Run contract. Every /api/** call requires a service token with audience lms-service.

---
+++ src/main/java/co/edu/icesi/student360/lms/LmsServiceApplication.java
@@ -0,0 +1,12 @@
+package co.edu.icesi.student360.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LmsServiceApplication.class, args);
+  }
+}
